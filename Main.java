362e7973f0e70ee3a468644a69af571898b699fa
loloslols <loloslols@nate.com>
2018-09-11 14:30:10 +0900

    이찬희 수정
    ㄱㄱ

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 	public static void main(String[] args) {
 System.out.println("Hello Web&App");
 System.out.println("hi");
-System.out.ptrinln("hi2");
+System.out.ptrinln("hi2321321312");
 }
 }
